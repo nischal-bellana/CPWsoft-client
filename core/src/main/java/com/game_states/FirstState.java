cc349e3967602dc2d492caa6f00c6ab12ca4f547
nischal-bellana <nischal.bellana@gmail.com>
2025-06-19 09:09:37 +0530

    Connects when pressed Enter

--- core/src/main/java/com/game_states/FirstState.java
+++ core/src/main/java/com/game_states/FirstState.java
@@ -8,6 +8,7 @@ import java.net.SocketAddress;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
@@ -18,14 +19,13 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 public class FirstState extends State{
-    
-	
-	
+
+
 	@Override
 	public void create(State prevst) {
 		// TODO Auto-generated method stub
 		super.create(prevst);
-		
+
 		createStage();
 	}
 
@@ -34,15 +34,15 @@ public class FirstState extends State{
     	Table table = new Table();
     	table.setBackground(skin.getDrawable("back"));
     	initStage(table);
-    	
+
     	Label l_username = new Label("Username", skin);
     	table.row();
     	table.add(l_username).padTop(18);
-    	
+
     	TextField name_tf = new TextField("",skin);
     	table.row();
     	table.add(name_tf).height(18).padTop(18);
-    	
+
     	Button connect_b = new Button(skin);
     	connect_b.addListener(new ChangeListener() {
 
@@ -54,20 +54,33 @@ public class FirstState extends State{
 				usernametext = usernametext.trim();
 				if(validateUsername(usernametext)) connectServer(usernametext);
 			}
-    		
+
     	});
     	table.row();
     	table.add(connect_b).center();
-    	
+
+        table.addListener(new InputListener(){
+            @Override
+            public boolean keyTyped(InputEvent event, char character) {
+                if(character == '\n'){
+                    System.out.println("Connecting...");
+                    String usernametext = name_tf.getText();
+                    usernametext = usernametext.trim();
+                    if(validateUsername(usernametext)) connectServer(usernametext);
+                }
+                return true;
+            }
+        });
+
     	Label l_connect = new Label("Connect to Server",skin);
     	table.row();
     	table.add(l_connect).center().padTop(18);
-    	
+
     	TextField server_ip_tf = new TextField("localhost",skin);
     	server_ip_tf.setName("serverip");
     	table.row();
     	table.add(server_ip_tf).height(18).padTop(18);
-    	
+
     	TextButton play_offline_tb = new TextButton("Play Offline", skin);
     	table.row();
 		table.add(play_offline_tb).padTop(50);
@@ -78,9 +91,9 @@ public class FirstState extends State{
 				// TODO Auto-generated method stub
 				changeState(new GameStateOffline());
 			}
-			
+
 		});
-    	
+
     	table.setTouchable(Touchable.enabled);
     	table.addListener(new ClickListener() {
 
@@ -88,57 +101,57 @@ public class FirstState extends State{
 			public void clicked(InputEvent event, float x, float y) {
 				// TODO Auto-generated method stub
 				if(stage.getKeyboardFocus() == null) return;
-				
+
 				if(event.getTarget()!= stage.getKeyboardFocus()) {
 					stage.unfocus(stage.getKeyboardFocus());
 				}
 			}
     	});
-    	
+
     }
 
 	public boolean validateUsername(String username) {
 		if(username.length() <= 5 || username.length() > 14 || Character.isDigit(username.charAt(0))) return false;
 		for(int i = 0; i < username.length(); i++) {
 			char c = username.charAt(i);
-			if(c != '-' && c != '_' && !Character.isAlphabetic(c) && !Character.isDigit(c)) 
+			if(c != '-' && c != '_' && !Character.isAlphabetic(c) && !Character.isDigit(c))
 				return false;
 		}
 		return true;
 	}
 
 	private void connectServer(String name) {
-    	try {  
+    	try {
     		if(serverbridge != null && serverbridge.isConnected()) serverbridge.closeSocket();
-    		
+
     		TextField tf = stage.getRoot().findActor("serverip");
-    		
+
     		SocketAddress address = new InetSocketAddress(tf.getText(), 1323);
     		Socket server = new Socket();
 
     		int timeout = 3000;
     		server.connect(address, timeout);
-			
+
     		serverbridge = new ServerBridge(server, name);
-    		
+
     		if(!serverbridge.isConnected()) {
     			serverbridge.closeSocket();
     			serverbridge = null;
     			System.out.println("Connection failed server socket is closed");
     			return;
     		}
-			
+
     		Thread serverbridgethread = new Thread(serverbridge);
     		serverbridgethread.start();
-    		
+
     		next_state_inf = new String[1];
 			next_state_inf[0] = name;
 			changeState(new LobbyState());
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.out.println("Connection failed");
 		}
     }
-    
+
 }
